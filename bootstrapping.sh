c2a736597a64fd35a795bc4a4f9c5cca5aeaf400
Jon D. <info@darkjon.de>
2025-11-04 01:45:40 +0100

    Update bootstrapping.sh

--- bootstrapping.sh
+++ bootstrapping.sh
@@ -14,7 +14,7 @@ fi
 cd ./bootstrap
 
 if [ ! -f ./bootstrap.zip ]; then
-   wget https://github.com/LIMXTEC/BitCore/releases/download/${VERSION}/bootstrap.zip -O bootstrap.zip
+   wget https://github.com/bitcore-btx/BitCore/releases/download/0.90.9.10/2024-10-10.Bootstrap.zip -O bootstrap.zip
 fi
 if [ ! -d ./blocks ]; then
    unzip bootstrap.zip
