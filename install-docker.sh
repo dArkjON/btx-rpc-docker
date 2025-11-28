11a024f05439d7cfc1cebc3535b93fa4939c8c1c
Jon D. <info@darkjon.de>
2025-11-28 11:35:58 +0100

    Update docker-compose installation to latest version

--- install-docker.sh
+++ install-docker.sh
@@ -19,9 +19,9 @@ sudo apt-get update
 sudo apt-get install docker-ce
 
 
-# Install docker-compose
+# Install docker-compose (latest version)
 # https://docs.docker.com/compose/install/#install-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
 
 echo "Installed Docker"
